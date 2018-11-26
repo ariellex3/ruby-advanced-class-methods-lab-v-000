b9169f4ee201761c636e295eec35661ad19cdaa3
Arielle D <domingu4@gmail.com>
2018-11-26 13:16:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -67,16 +67,3 @@ class Song
   end
 
 end
-
-def self.new_by_name(name)
-  song = self.new
-  song.name = name
-  song
-end
-
-def self.create_by_name(name)
-  song = self.new
-  song.name = name
-  song.save
-  song
-end
